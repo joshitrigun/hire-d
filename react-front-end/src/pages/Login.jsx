ccd440fdc0ddfcf0e037cd65df57d3d748a8ccbe
Mohamed Ali <12maali27@gmail.com>
2022-02-14 01:56:11 +0000

    create login page

---
+++ react-front-end/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <h3>THIS IS LOGIN PAGE</h3>
+    </div>
+  );
+};
+
+export default Login;
