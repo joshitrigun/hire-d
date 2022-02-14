3a45c39ab357e74751773479fb2d7d72e6dcefb8
Mohamed Ali <12maali27@gmail.com>
2022-02-14 01:55:54 +0000

    create projects page

---
+++ react-front-end/src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Projects = () => {
+  return (
+    <div>
+      <Header />
+      <h3>THIS IS THE PROJECT PAGE</h3>
+    </div>
+  );
+};
+
+export default Projects;
