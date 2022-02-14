4a64b40da66132a86af2deddbe4719e2ce0ba0e5
Mohamed Ali <12maali27@gmail.com>
2022-02-14 02:07:33 +0000

    add require statement for dotenv

--- express-back-end/database/connection.js
+++ express-back-end/database/connection.js
@@ -1,4 +1,5 @@
-const pg = require('pg');
+require("dotenv").config();
+const pg = require("pg");
 const Client = pg.Client;
 
 const config = {
@@ -6,12 +7,12 @@ const config = {
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT
-}
+  port: process.env.DB_PORT,
+};
 const client = new Client(config);
 
 client.connect(() => {
-  console.log('connected to the database');
+  console.log("connected to the database");
 });
 
 module.exports = client;
