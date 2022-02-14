4e7ba171adaa0d871825c61ee538d1707f9a72ee
Mohamed Ali <12maali27@gmail.com>
2022-02-14 22:04:55 +0000

    change projects path

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Jobs from "./pages/Jobs";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Projects from "./pages/Projects";
 import Developers from "./pages/Developers";
@@ -11,11 +11,11 @@ import Login from "./pages/Login";
 const App = () => {
   return (
     <Routes>
-      <Route exact path="/" element={<Homepage />} />
-      <Route exact path="/jobs" element={<Jobs />} />
-      <Route exact path="/projects" element={<Projects />} />
-      <Route exact path="/developers" element={<Developers />} />
-      <Route exact path="/login" element={<Login />} />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/jobs" element={<Jobs />} />
+      <Route path="projects/*" element={<Projects />} />
+      <Route path="/developers" element={<Developers />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
