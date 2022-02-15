718375f3c4b7e0acf793b0bf1e1fd8f010b9e036
Mohamed Ali <12maali27@gmail.com>
2022-02-15 20:20:24 +0000

    remove commented out code

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -22,14 +22,6 @@ App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static("public"));
 App.use(cors(corsOptions));
-// App.use(cors(), function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 // Sample GET route
 App.get("/api/users", getAllUsers);
