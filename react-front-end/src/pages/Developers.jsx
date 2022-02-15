cf855ca3a53c689ad04ba97268c994bc0090cb5f
Md Hefzul Bari <ratul14april@gmail.com>
2022-02-14 19:54:57 -0600

    New queries, new api endpoint, project page, project detail page

--- react-front-end/src/pages/Developers.jsx
+++ react-front-end/src/pages/Developers.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import TopNavBar from "../components/Top_nav_bar";
 
 const Developers = () => {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/user_projects')
+    .then((response) => {
+      setState(response.data)
+    })
+  }, []);
+
+
+  console.log(state);
+  // const projectsByUserId = (state, id) => {
+  // const projectsOfUser = state.filter((project) => project.owner_id === id)
+
+  //   return projectsOfUser;
+  // }
+
   return (
     <div>
       <TopNavBar />
