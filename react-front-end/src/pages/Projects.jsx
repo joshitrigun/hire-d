b6793d3601f4c487421dcaf06699b771cd43b2ab
Mohamed Ali <12maali27@gmail.com>
2022-02-15 20:17:54 +0000

    removed imports that weren't being used in file

--- react-front-end/src/pages/Projects.jsx
+++ react-front-end/src/pages/Projects.jsx
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import CreateProject from "../components/CreateProject";
 import AllProjects from "../components/AllProjects";
-import axios from "axios";
 import TopNavBar from "../components/Top_nav_bar";
 import "../components/ProjectListItem.css";
-import ProjectListItem from "../components/ProjectListItem";
 //import ProjectPopup from "../helpers/modal";
 
 const Projects = () => {
