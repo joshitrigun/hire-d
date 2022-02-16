0fa2248b5712ba0f96a978e9f3f44be3a829b447
Mohamed Ali <12maali27@gmail.com>
2022-02-16 02:53:17 +0000

    create login component

--- react-front-end/src/pages/Login.jsx
+++ react-front-end/src/pages/Login.jsx
@@ -1,11 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import TopNavBar from "../components/Top_nav_bar";
 
 const Login = () => {
   return (
     <div>
       <TopNavBar />
-      <h3>THIS IS LOGIN PAGE</h3>
+      <form className="w-25 mt-5 mx-auto">
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3">
+          Not a user?{" "}
+          <span>
+            <Link to={"/signup"}>Click here to register</Link>
+          </span>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
