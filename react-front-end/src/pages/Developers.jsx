8f31952d2e9d4650f865e42ac64ca9c3ccc64f42
Mohamed Ali <12maali27@gmail.com>
2022-02-18 00:23:49 +0000

    add new route when trying to edit profile

--- react-front-end/src/pages/Developers.jsx
+++ react-front-end/src/pages/Developers.jsx
@@ -4,12 +4,13 @@ import { Route, Routes } from "react-router-dom";
 import DeveloperList from "../components/developers/DeveloperList";
 import DeveloperDetail from "../components/developers/DeveloperDetail";
 import CreateCertification from "../components/certifications/CreateCertification";
+import CreateSeekerForm from "../components/forms/CreateSeekerForm";
 
 const Developers = () => {
   return (
     <div className="main">
       <TopNavBar />
-      <h2 className="page-title">Developers</h2>
+      {/* <h2 className="page-title">Developers</h2> */}
       <Routes>
         <Route path="/" element={<DeveloperList />} />
         <Route path=":id" element={<DeveloperDetail />} />
@@ -17,6 +18,7 @@ const Developers = () => {
           path=":id/certifications/new"
           element={<CreateCertification />}
         />
+        <Route path=":id/profile/edit" element={<CreateSeekerForm />} />
       </Routes>
     </div>
   );
