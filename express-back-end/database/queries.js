faa78b34eaed55cab345869f13d3abb1afbae667
Mohamed Ali <12maali27@gmail.com>
2022-02-19 22:38:43 +0000

    add order by id when getting all job seekers and employers

--- express-back-end/database/queries.js
+++ express-back-end/database/queries.js
@@ -3,7 +3,7 @@ const pg = require("pg");
 const client = require("./connection");
 
 const getAllUsers = (request, response) => {
-  const queryString = "SELECT * FROM users WHERE employer = false;";
+  const queryString = "SELECT * FROM users WHERE employer = false ORDER BY id;";
 
   client.query(queryString, (error, results) => {
     if (error) {
@@ -25,7 +25,7 @@ const getUser = (request, response) => {
 };
 
 const getAllEmployers = (request, response) => {
-  const queryString = "SELECT * FROM users WHERE employer = true;";
+  const queryString = "SELECT * FROM users WHERE employer = true ORDER BY id;";
 
   client.query(queryString, (error, results) => {
     if (error) {
