7ccc3e930934d7338a9dc0abf8a2c1e91fb68a74
Md Hefzul Bari <ratul14april@gmail.com>
2022-02-19 22:56:49 -0600

    Update Employer Form

--- react-front-end/src/components/forms/EditEmployerForm.jsx
+++ react-front-end/src/components/forms/EditEmployerForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-// import { useParams, useLocation, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
