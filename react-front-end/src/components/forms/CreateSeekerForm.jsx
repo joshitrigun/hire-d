e9ab148dca3a74cfb5947ea71f759dfed7b87aaf
Mohamed Ali <12maali27@gmail.com>
2022-02-20 02:55:29 +0000

    change toString() to join() and add styling for text when form submitted

--- react-front-end/src/components/forms/CreateSeekerForm.jsx
+++ react-front-end/src/components/forms/CreateSeekerForm.jsx
@@ -5,9 +5,9 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import "./CreateSeekerForm.module.css";
+import "./CreateSeekerForm.css";
 
-const CreateStudentForm = () => {
+const CreateSeekerForm = () => {
   const navigate = useNavigate();
   const techArray = tech_stack.map((skill) => {
     return { ...skill, checked: false };
@@ -185,7 +185,7 @@ const CreateStudentForm = () => {
       resume,
       avatar,
       employer: false,
-      skills: stack.toString(),
+      skills: stack.join(", "),
     };
     console.log(data);
     const id = Number(Cookies.get("id"));
@@ -204,174 +204,193 @@ const CreateStudentForm = () => {
 
   return (
     <>
-      {submitted ? <p className="text-center">{submitted}</p> : ""}
-      {error ? <p className="text-center">{error}</p> : ""}
       <form className="w-100 mx-auto" onSubmit={(e) => e.preventDefault()}>
         <div className="form-container">
-          <div className="form-header">
-            <h3 className="text-center">
-              {Cookies.get("user") ? "Edit" : "Create"} Profile
-            </h3>
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="First Name"
-                name="first_name"
-                value={firstName}
-                onChange={(event) => setFirstName(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="Last Name"
-                name="last_name"
-                value={lastName}
-                onChange={(event) => setLastName(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="Phone Number"
-                name="number"
-                value={number}
-                onChange={(event) => setNumber(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="password"
-                placeholder="Confirm Password"
-                name="confirm_password"
-                value={confirmPassword}
-                onChange={(event) => setConfirmPassword(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="Designation"
-                name="designation"
-                value={designation}
-                onChange={(event) => setDesignation(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <textarea
-                name="about"
-                rows="5"
-                cols="33"
-                placeholder="About me..."
-                value={about}
-                onChange={(event) => setAbout(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="City"
-                name="city"
-                value={city}
-                onChange={(event) => setCity(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="Province"
-                name="province"
-                value={province}
-                onChange={(event) => setProvince(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="Github"
-                name="github_url"
-                value={githubUrl}
-                onChange={(event) => setGithubUrl(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="Linkedin"
-                name="linkedin_url"
-                value={linkedinUrl}
-                onChange={(event) => setLinkedinUrl(event.target.value)}
-              />
-            </div>
-            {/* COME BACK TO THIS BELOW  */}
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="Choose Avatar"
-                name="avatar"
-                value={avatar}
-                onChange={(event) => setAvatar(event.target.value)}
-              />
-            </div>
-            <div className="form-input">
-              <input
-                type="text"
-                placeholder="Upload Resume"
-                name="resume"
-                value={resume}
-                onChange={(event) => setResume(event.target.value)}
-              />
-            </div>
+          <div className="message">
+            {submitted ? (
+              <p className="bg-success text-center text-white w-25 mx-auto fw-bold">
+                {submitted}
+              </p>
+            ) : (
+              ""
+            )}
+            {error ? (
+              <p className="bg-danger text-center text-white w-25 mx-auto fw-bold">
+                {error}
+              </p>
+            ) : (
+              ""
+            )}
           </div>
-          <div className="form-footer">
-            <div className="d-flex flex-column">
-              <h4>Add Skills</h4>
-              <div className="tech-stack">
-                {checkedState.map(({ name }, index) => {
-                  return (
-                    <div className="tech-stack-names" key={index}>
-                      <input
-                        type="checkbox"
-                        name={name}
-                        value={name}
-                        id={name}
-                        checked={checkedState[index].checked}
-                        onChange={() => onChangeHandler(index)}
-                      />
-                      <label htmlFor={name}>{name}</label>
-                    </div>
-                  );
-                })}
+          <div className="forms">
+            <div className="form-header">
+              <h3 className="text-center">Edit Profile</h3>
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  name="first_name"
+                  value={firstName}
+                  onChange={(event) => setFirstName(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  name="last_name"
+                  value={lastName}
+                  onChange={(event) => setLastName(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="Phone Number"
+                  name="number"
+                  value={number}
+                  onChange={(event) => setNumber(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="confirm_password"
+                  value={confirmPassword}
+                  onChange={(event) => setConfirmPassword(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="Designation"
+                  name="designation"
+                  value={designation}
+                  onChange={(event) => setDesignation(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <textarea
+                  name="about"
+                  rows="5"
+                  cols="33"
+                  placeholder="About me..."
+                  value={about}
+                  onChange={(event) => setAbout(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="City"
+                  name="city"
+                  value={city}
+                  onChange={(event) => setCity(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="Province"
+                  name="province"
+                  value={province}
+                  onChange={(event) => setProvince(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="Github"
+                  name="github_url"
+                  value={githubUrl}
+                  onChange={(event) => setGithubUrl(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="Linkedin"
+                  name="linkedin_url"
+                  value={linkedinUrl}
+                  onChange={(event) => setLinkedinUrl(event.target.value)}
+                />
+              </div>
+              {/* COME BACK TO THIS BELOW  */}
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="Choose Avatar"
+                  name="avatar"
+                  value={avatar}
+                  onChange={(event) => setAvatar(event.target.value)}
+                />
+              </div>
+              <div className="form-input">
+                <input
+                  type="text"
+                  placeholder="Upload Resume"
+                  name="resume"
+                  value={resume}
+                  onChange={(event) => setResume(event.target.value)}
+                />
               </div>
             </div>
-            <Stack spacing={2} direction="row">
-              <Button onClick={validate}>Save</Button>
-              <Link
-                to={
-                  Cookies.get("user") ? `/developers/${Cookies.get("id")}` : "/"
-                }
-              >
-                <Button>Cancel</Button>
-              </Link>
-            </Stack>
+            <div className="form-footer">
+              <div className="d-flex flex-column">
+                <h4>Add Skills</h4>
+                <div className="tech-stack">
+                  {checkedState.map(({ name }, index) => {
+                    return (
+                      <div className="tech-stack-names" key={index}>
+                        <input
+                          type="checkbox"
+                          name={name}
+                          value={name}
+                          id={name}
+                          checked={checkedState[index].checked}
+                          onChange={() => onChangeHandler(index)}
+                        />
+                        <label htmlFor={name}>{name}</label>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <Stack spacing={2} direction="row">
+                <Button variant="outlined" onClick={validate}>
+                  Save
+                </Button>
+                <Button
+                  variant="outlined"
+                  href={
+                    Cookies.get("user")
+                      ? `/developers/${Cookies.get("id")}`
+                      : "/"
+                  }
+                >
+                  Cancel
+                </Button>
+              </Stack>
+            </div>
           </div>
         </div>
       </form>
@@ -379,4 +398,4 @@ const CreateStudentForm = () => {
   );
 };
 
-export default CreateStudentForm;
+export default CreateSeekerForm;
