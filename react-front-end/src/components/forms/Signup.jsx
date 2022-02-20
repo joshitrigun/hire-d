1ba64f6a81da46a1f6dbb4d9d73cbe9b9fa83cb3
Mohamed Ali <12maali27@gmail.com>
2022-02-20 20:51:37 +0000

    styling changes

--- react-front-end/src/components/forms/Signup.jsx
+++ react-front-end/src/components/forms/Signup.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./Signup.module.css";
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 const Signup = () => {
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center pt-5">
+    <div className="d-flex flex-column justify-content-center align-items-center pt-5 mx-auto">
       <h2 className="pb-5 fs-2">Are you:</h2>
       <Stack spacing={2} direction="row">
         <Button variant="outlined" href={"/signup/seeker"}>
-            Job Seeker
+          Job Seeker
         </Button>
         <h4>or</h4>
         <Button variant="outlined" href={"/signup/employer"}>
-            Employer
+          Employer
         </Button>
       </Stack>
     </div>
