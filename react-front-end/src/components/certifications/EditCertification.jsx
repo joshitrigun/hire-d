6540b4f490d21d7f7a64310dead6be09cb363a9b
Mohamed Ali <12maali27@gmail.com>
2022-02-21 01:38:28 +0000

    remove unused code

--- react-front-end/src/components/certifications/EditCertification.jsx
+++ react-front-end/src/components/certifications/EditCertification.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams, useLocation, useNavigate } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie";
 import "./EditCertification.css";
