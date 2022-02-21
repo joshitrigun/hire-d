32a9a72edd5055fd9a9eaa2a636c26f080551428
Mohamed Ali <12maali27@gmail.com>
2022-02-21 01:02:49 +0000

    add condition to render edit btn

--- react-front-end/src/components/jobs/JobListItem.jsx
+++ react-front-end/src/components/jobs/JobListItem.jsx
@@ -7,8 +7,17 @@ import Button from "@mui/material/Button";
 import Cookies from "js-cookie";
 
 const JobListItem = (props) => {
-  const { id, title, employer, city, province, salary, apply_link, jobType } =
-    props;
+  const {
+    id,
+    title,
+    employer,
+    city,
+    province,
+    salary,
+    apply_link,
+    jobType,
+    employerId,
+  } = props;
 
   let navigate = useNavigate();
   let location = useLocation();
@@ -18,9 +27,9 @@ const JobListItem = (props) => {
   };
 
   return (
-    <div className="job-block" onClick={routeChange}>
+    <div className="job-block">
       <span className="icon-frame">
-        <HiBriefcase className="hi-icon" />
+        <HiBriefcase className="hi-icon" onClick={routeChange} />
       </span>
       <span className="job-info">
         <NavLink className="title-link" to={`/jobs/${id}`}>
@@ -38,7 +47,8 @@ const JobListItem = (props) => {
         <Button variant="outlined" href={apply_link} target="_blank">
           Apply Here
         </Button>
-        {Cookies.get("employer") === "true" && location.pathname !== "/jobs" ? (
+        {location.pathname !== "/jobs" &&
+        Number(Cookies.get("id")) === employerId ? (
           <span className="ms-2">
             <Button variant="outlined" href={`/jobs/${id}/edit`}>
               Edit
