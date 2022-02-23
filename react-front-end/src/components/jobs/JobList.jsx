b4210d1fa6904e8dbd369d2f1fb8fdedad04a313
Mohamed Ali <12maali27@gmail.com>
2022-02-23 01:44:50 +0000

    remove console logs

--- react-front-end/src/components/jobs/JobList.jsx
+++ react-front-end/src/components/jobs/JobList.jsx
@@ -8,7 +8,6 @@ const JobList = () => {
 
   useEffect(() => {
     axios.get("/api/jobs_employers").then((response) => {
-      console.log(response.data);
       setJobs(response.data);
     });
   }, []);
